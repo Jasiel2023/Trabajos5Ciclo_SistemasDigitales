f6d12c8b2723170487d3e794e38aa4b41319c90b
Jasiel <148463902+Jasiel2023@users.noreply.github.com>
2026-04-15 07:33:57 -0500

    Add files via upload

---
+++ APE1/APE1.ino
@@ -0,0 +1,120 @@
+const int LED_PINS[]={2,3,4,5,6,7};
+const int BOTON_PIN= 8;
+int patronActual = 0;
+void setup()
+{
+  for(int i=0; i<= 5; i++){
+  	pinMode(LED_PINS[i], OUTPUT);
+  }
+  pinMode(BOTON_PIN, INPUT);
+}
+
+void loop()
+{
+  leerBoton();
+  switch (patronActual){
+    case 0:
+     patronSecuencia();
+     break;
+    case 1:
+     patronPersecucion();
+     break;
+     case 2:
+     patronParpadeo();
+     break;
+     case 3:
+     patronAleatorio();
+     break;
+     case 4:
+     patronOnda();
+     break;
+  } 
+}
+
+void leerBoton(){
+  if(digitalRead(BOTON_PIN) == HIGH){
+    patronActual++;
+    if(patronActual > 4){
+      patronActual = 0;
+    }
+    delay(300);
+  }else{
+    patronActual=0;
+    for(int i=0;i <= 5; i++){
+    digitalWrite(LED_PINS[i], LOW);
+  }
+  }
+}
+
+void patronSecuencia(){
+  for(int i=0;i <= 5; i++){
+    digitalWrite(LED_PINS[i], HIGH);
+    delay(150);
+  }
+
+  for(int i=0;i <= 5; i++){
+    digitalWrite(LED_PINS[i], LOW);
+    delay(150);
+  }
+
+  
+}
+
+void patronPersecucion(){
+  for(int i=0;i <= 5; i++){
+    digitalWrite(LED_PINS[i], HIGH);
+    delay(150);
+    digitalWrite(LED_PINS[i], LOW);
+  }
+}
+
+void patronParpadeo(){
+  int cont = 0;
+   while(cont < 4){
+       for(int i=0;i <= 5; i++){
+        digitalWrite(LED_PINS[i], HIGH);
+      }
+    delay(300);
+    for(int i=0;i <= 5; i++){
+        digitalWrite(LED_PINS[i], LOW);
+      }
+      delay(300);
+      cont++;
+   }
+}
+
+void patronAleatorio(){
+  for(int i=0;i <= 5; i++){
+    int aleatorio = random(0, 2);
+        if(aleatorio == 0){
+          digitalWrite(LED_PINS[i], LOW);
+        }else{
+          digitalWrite(LED_PINS[i], HIGH);
+        }
+        
+   }
+    delay(300);
+    for(int i=0;i <= 5; i++){
+        digitalWrite(LED_PINS[i], LOW);
+      }
+}
+
+void patronOnda(){
+   for(int i=0;i <= 5; i++){
+        digitalWrite(LED_PINS[i], HIGH);
+        delay(200);
+        digitalWrite(LED_PINS[i], LOW);
+      }
+    
+   for(int i=5;i >= 0; i--){
+        digitalWrite(LED_PINS[i], HIGH);
+        delay(200);
+        digitalWrite(LED_PINS[i], LOW);
+      }
+}
+
+
+
+
+
+
